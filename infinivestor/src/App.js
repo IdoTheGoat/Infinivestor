f4e806e3df01dddd80873e231398720517c92b1a
Ido Tal <ital665@student.fuhsd.org>
2021-02-02 09:33:26 -0800

    cool

---
+++ infinivestor/src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+
+          <div class = 'main_frame'>
+
+              <div class = 'left_frame'> 
+              
+            
+                 
+              </div>
+
+          <div class='right_frame'>
+
+            
+
+          </div>
+
+          </div>
+
+      </header>
+
+    </div>
+
+  );
+
+}
+
+export default App;
